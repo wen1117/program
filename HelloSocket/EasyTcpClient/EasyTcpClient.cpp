9475e24f9179ec515638ab4221c64af077e1fe5f
wen1117 <670367713@qq.com>
2021-06-03 00:08:55 +0800

    client跨平台移植

--- HelloSocket/EasyTcpClient/EasyTcpClient.cpp
+++ HelloSocket/EasyTcpClient/EasyTcpClient.cpp
@@ -159,7 +159,7 @@ int main() {
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(4567);
 #ifdef _WIN32
-	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");//连接的服务器地址
 #else
 	_sin.sin_addr.s_addr = inet_addr("192.168.17.1");
 #endif // _WIN32	
